3f78dfb66a2a286e92ee600a5453b0afecc53aee
todai88 <joabaj88@gmail.com>
2017-10-05 22:18:55 +0100

    Finished Day003, Now off to QA

--- Code/win32_handmade.cpp
+++ Code/win32_handmade.cpp
@@ -1,5 +1,97 @@
 #include <windows.h> 
+#include <stdint.h>
 
+#define local_persist static
+#define global_variable static
+#define internal static
+
+typedef uint8_t uint8; 
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8; 
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory; 
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
+/*
+    At Day 003 1:06:21
+*/
+internal void
+RenderWeirdGradient(int XOffset, int YOffset)
+{
+    
+    int Width = BitmapWidth;
+    int Height = BitmapHeight;
+
+    
+    int Pitch = Width * BytesPerPixel;
+	uint8 *Row = (uint8 *)BitmapMemory;
+
+    for(int WindowRow = 0; WindowRow < BitmapHeight; ++WindowRow)
+    {
+        uint32 *Pixel = (uint32 *)Row;
+
+        for(int WindowCol = 0; WindowCol < BitmapWidth; ++WindowCol)
+        {
+			uint8 Blue  = (WindowCol + XOffset);
+            uint8 Green = (WindowRow + YOffset);
+            
+            *Pixel++ = ((Green << 8) | Blue);
+        }
+        Row += Pitch;   
+    }
+}
+internal void Win32ResizeDIBSection(int Width, int Height)
+{ 
+    if (BitmapMemory)
+    {
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+    }
+
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB; 
+
+    int BytesPerPixel = 4;
+    int BitmapMemorySize = (BitmapWidth * BitmapHeight) * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize,
+                                MEM_COMMIT, PAGE_READWRITE);
+    RenderWeirdGradient(0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, 
+                                RECT *ClientRect,
+                                int X, int Y, int Width, int Height)
+{
+    int WindowWidth = ClientRect->right - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
+
+    StretchDIBits(
+        DeviceContext, 
+        /*
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        */
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        SRCCOPY);
+}
 LRESULT CALLBACK MainWindowCallback(HWND Window,
                    UINT Message,
                    WPARAM WParam,
@@ -14,18 +106,26 @@ LRESULT CALLBACK MainWindowCallback(HWND Window,
         }
         case WM_SIZE:
         {
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Height = ClientRect.bottom - ClientRect.top;
+            int Width = ClientRect.right - ClientRect.left;   
+            Win32ResizeDIBSection(Width, Height);
             OutputDebugStringA("WM_SIZE\n");
             break;  
         }
 
         case WM_DESTROY:
         {
+            Running = false;
             OutputDebugStringA("WM_DESTROY\n");
             break;
         }
 
         case WM_CLOSE:
         {
+            Running = false;
+            PostQuitMessage(0);
             OutputDebugStringA("WM_CLOSE\n");
             break;
         }
@@ -39,20 +139,15 @@ LRESULT CALLBACK MainWindowCallback(HWND Window,
             PAINTSTRUCT Paint;
             OutputDebugStringA("WM_PAINT\n");
             HDC DeviceContext =  BeginPaint(Window, &Paint); 
+
             int X = Paint.rcPaint.left;
             int Y = Paint.rcPaint.top;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left; 
-            static DWORD Operation = WHITENESS;
-            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-            if (Operation == WHITENESS)
-            {
-                Operation = BLACKNESS;
-            }
-            else
-            {
-                Operation = WHITENESS;
-            }
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint); 
             break;
         }
@@ -94,20 +189,34 @@ int CALLBACK WinMain(
                             0);
         if (Window)
         {
+            int XOffset = 0;
+            int YOffset = 0;
+            Running = true;
             MSG Message;
-            for (;;)
+            while(Running)
             {
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0) 
+                MSG Message;
+                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
+                    if (Message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
                 }
-                else 
-                {
-                    // message is falsy
-                    break;
-                }
+                
+                RenderWeirdGradient(XOffset, YOffset);
+                
+                HDC DeviceContext = GetDC(Window);
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+
+                ReleaseDC(Window, DeviceContext);
+                ++XOffset;
             }
         }
         else 
